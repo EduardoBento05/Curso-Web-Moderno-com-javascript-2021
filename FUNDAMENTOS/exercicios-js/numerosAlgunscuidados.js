bce3e2901a3d4837b95b8c6a821c567e130625e1
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2021-11-04 18:54:37 -0300

    Add files via upload

---
+++ FUNDAMENTOS/exercicios-js/numerosAlgunscuidados.js
@@ -0,0 +1,6 @@
+console.log(7/0.0000001)//retorna infinito
+console.log("10"/2)// em js o resultado da 5
+console.log("Show!"*2)//não funciona o resultado da Not a Number
+console.log(0.1+0.7)//não retorna 0.8 , retorna 0.7999999...
+//console.log(10.toString())
+console.log((10.345).toFixed(2))//arredonda o numero
