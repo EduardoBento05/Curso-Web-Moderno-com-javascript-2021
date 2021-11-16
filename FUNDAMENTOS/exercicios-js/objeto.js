5c1fe3e2ab0a4113417f1fe05a6c97efd56eba90
Eduardo Bento <80079390+duzin12@users.noreply.github.com>
2021-11-16 19:53:57 -0300

    Add files via upload

---
+++ FUNDAMENTOS/exercicios-js/objeto.js
@@ -0,0 +1,13 @@
+const prod1  = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 5000.00
+prod1['desconto legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 39.90
+}
+
+console.log(prod2)
